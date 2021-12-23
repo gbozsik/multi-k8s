6f0f098cf47f0e86d02f1bd2cfeec1f0e8518046
gbozsik <g.bozsik@gmail.com>
2021-12-23 17:51:35 +0100

    change again

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React Modified
+            Learn React Modified again
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>

740557ec345e882517747bc174358377c3b7598c
gbozsik <g.bozsik@gmail.com>
2021-12-23 18:02:58 +0100

    change image name

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ docker push gbozsik/multi-server:latest
 docker push gbozsik/multi-worker:latest
 
 docker push gbozsik/multi-client:$SHA
-docker push gbozsik/multi-server:$SHA
+docker push gbozsik/multi-server:sanyi
 docker push gbozsik/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=gbozsik/multi-server$SHA
+kubectl set image deployments/server-deployment server=gbozsik/multi-server:sanyi
 kubectl set image deployments/client-deployment client=gbozsik/multi-client:latest
 kubectl set image deployments/worker-deployment worker=gbozsik/multi-worker$SHA

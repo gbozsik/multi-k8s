457130dbdf33639d45d36782a0bcb46a08d21752
gbozsik <g.bozsik@gmail.com>
2021-12-23 18:23:39 +0100

    and again change deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker build -t gbozsik/multi-client:latest -t gbozsik/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t gbozsik/multi-server:latest -t gbozsik/multi-server:sanyi -f ./server/Dockerfile ./server
+docker build -t gbozsik/multi-server:latest -t gbozsik/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t gbozsik/multi-worker:latest -t gbozsik/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push gbozsik/multi-client:latest
@@ -7,10 +7,10 @@ docker push gbozsik/multi-server:latest
 docker push gbozsik/multi-worker:latest
 
 docker push gbozsik/multi-client:$SHA
-docker push gbozsik/multi-server:sanyi
+docker push gbozsik/multi-server:$SHA
 docker push gbozsik/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=gbozsik/multi-server:sanyi
-kubectl set image deployments/client-deployment client=gbozsik/multi-client:latest
-kubectl set image deployments/worker-deployment worker=gbozsik/multi-worker$SHA
+kubectl set image deployments/server-deployment server=gbozsik/multi-server:$SHA
+kubectl set image deployments/client-deployment client=gbozsik/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=gbozsik/multi-worker:$SHA

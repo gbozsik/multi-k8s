3b1c330b2f5fac80e2b8019628fce917d743e31d
gbozsik <g.bozsik@gmail.com>
2021-12-23 13:45:00 +0100

    Added deployment scripts

---
+++ client/deploy.sh
@@ -0,0 +1,16 @@
+docker build -t gbozsik/multi-client:latest -t gbozsik/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t gbozsik/multi-server:latest -t gbozsik/multi-server:$SHA -f ./server/Dockerfile ./server
+docker biuld -t gbozsik/multi-worker:latest -t gbozsik/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push gbozsik/multi-client:latest
+docker push gbozsik/multi-server:latest
+docker push gbozsik/multi-worker:latest
+
+docker push gbozsik/multi-client:$SHA
+docker push gbozsik/multi-server:$SHA
+docker push gbozsik/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=gbozsik/multi-server$SHA
+kubectl set image deployments/client-deployment client=gbozsik/multi-client$SHA
+kubectl set image deployments/worker-deployment worker=gbozsik/multi-worker$SHA
